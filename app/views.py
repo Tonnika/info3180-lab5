f467ca368ebb8211d299e795738952ce0cc41006
Tonnika Richards <shantiwilliams11@gmail.com>
2019-03-04 03:16:19 +0000

    ex 5

--- app/views.py
+++ app/views.py
@@ -63,6 +63,12 @@ def login():
 def secure_page():
     return render_template('secure_page.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("Logged out")
+    return redirect(url_for('home'))
+
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
